ee902c7ca1882841033f6ed879f237d77cb3f423
Jackson Reynolds <jwreynold@gmail.com>
2020-07-07 15:09:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end #badge_maker
+
+def batch_badge_creator(name_array)
+  badge_array = []
+  name_array.each do |name|
+    badge_array.push(badge_maker(name))
+  end #each
+  badge_array
+end #batch_badge_creator
